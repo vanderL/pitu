1c5cea79b2e879abd41f119102248281a21856d1
vanderL <vnd.vander@gmail.com>
2020-11-28 22:55:49 -0300

    Config database with sequelize and validate using typerscript resources

--- backend/src/models/link.ts
+++ backend/src/models/link.ts
@@ -4,3 +4,4 @@ export type Link = {
     code?: string,
     hits?: number
 }
+//schemae e suas regras
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,8 @@
 import app from './app'
+import database from './database'
 
+database.sync({force: true})
+console.log('Ha Ha Welcome to database my Lord, running at 3306')
 
 app.listen(3000)
 console.log('Good night my own! I am running at 3000')
