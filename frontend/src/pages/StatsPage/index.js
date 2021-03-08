223ed9850ead57cacdea8b3716290b36ea2a0dc0
vanderL <vnd.vander@gmail.com>
2021-03-07 23:23:51 -0300

    StatsPage: LifeCycle-React, Stats and Handle date

--- frontend/src/pages/StatsPage/index.js
+++ frontend/src/pages/StatsPage/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Header from '../../components/Header'
 import { Container } from 'react-bootstrap'
+
 import ShortenerService from '../../service/shortenerService'
+
+import { parseISO, formatRelative } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR' 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { StatsContainer, StatsBox, StatsRow, StatsBoxTitle } from './styles'
@@ -15,11 +19,31 @@ class StatsPage extends React.Component {
         this.state = {
             isLoading: false,
             shortenedUrl: {},
-            errorMessage: 'error viu!'
+            errorMessage: ''
         }
     }
 
+    async componentDidMount() {
+        const { code } = this.props.match.params
 
+        try {
+            const service = new ShortenerService()
+            const shortenedUrl = await service.getStats(code)
+
+            const parsedDate = parseISO(shortenedUrl.updated)
+            const currentDate = new Date()
+
+            const relativeDate = formatRelative(parsedDate, currentDate, {
+                locale: ptBR,
+            })
+
+            shortenedUrl.relativeDate = relativeDate
+
+            this.setState({isLoading: false, shortenedUrl})
+        } catch (error) {
+            this.setState({ isLoading: false, errorMessage: 'Ops, a URL não existe'})
+        }
+    }    
 
     render() {
         const { errorMessage, shortenedUrl } = this.state
@@ -34,8 +58,24 @@ class StatsPage extends React.Component {
                         <a className="btn btn-primary" href="/"> Encurtar nova URL</a>
                     </StatsContainer>
                 ) : (
-                    <p>Resultado</p>
+                    <StatsContainer className="text-center">
+                        <p><b> https://pitu.tk/{shortenedUrl.code} </b></p>
+                        <p>Redireciona para: <br /> {shortenedUrl.url}</p>
+                        <StatsRow>
+                            <StatsBox>
+                                <b> {shortenedUrl.hits} </b>
+                                <StatsBoxTitle> Visitas </StatsBoxTitle>
+                            </StatsBox>
+                            <StatsBox>
+                                <b> {shortenedUrl.relativeDate} </b>
+                                <StatsBoxTitle> Última visita </StatsBoxTitle>
+                            </StatsBox>
+                        </StatsRow>
+                        <a className="btn btn-primary" href="/"> Encurtar nova URL</a>
+                    </StatsContainer>
+
                 )
+                
 
                 }
             </Container>
